a2f4442d3f9cc0ae91dd714f3511146f17d64805
tronghuu.dailyxe <tronghuu.dailyxe@outlook.com>
2022-11-03 08:57:11 +0700

    ok

--- APIMyMyStore/Program.cs
+++ APIMyMyStore/Program.cs
@@ -12,7 +12,7 @@ builder.Configuration.AddConfiguration(configurationBuilder.Build());
 
 // Add services to the container.
 
-var defaultConnectionString = string.Empty;
+var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 if (builder.Environment.EnvironmentName == "Development")
 {
@@ -20,18 +20,23 @@ if (builder.Environment.EnvironmentName == "Development")
 }
 else
 {
-    // Use connection string provided at runtime by Heroku.
     var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
-    var userPassSide = connectionUrl.Split("@")[0];
-    var hostSide = connectionUrl.Split("@")[1];
 
-    var user = userPassSide.Split(":")[0];
-    var password = userPassSide.Split(":")[1];
-    var host = hostSide.Split("/")[0];
-    var database = hostSide.Split("/")[1].Split("?")[0];
+    if (connectionUrl != null)
+    {
+        // Use connection string provided at runtime by Heroku.
+        connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+        var userPassSide = connectionUrl.Split("@")[0];
+        var hostSide = connectionUrl.Split("@")[1];
 
-    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+        var user = userPassSide.Split(":")[0];
+        var password = userPassSide.Split(":")[1];
+        var host = hostSide.Split("/")[0];
+        var database = hostSide.Split("/")[1].Split("?")[0];
+
+        defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+
+    }
 }
 
 builder.Services.AddDbContext<ToDoContext>(options =>
