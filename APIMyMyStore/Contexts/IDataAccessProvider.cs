7d351c092de1ebc209db564008027f8ce2a5da22
tronghuu.dailyxe <tronghuu.dailyxe@outlook.com>
2022-11-04 16:13:20 +0700

    ok

--- APIMyMyStore/Contexts/IDataAccessProvider.cs
+++ APIMyMyStore/Contexts/IDataAccessProvider.cs
@@ -5,17 +5,21 @@ namespace APIMyMyStore.DataAccess
 {  
     public interface IDataAccessProvider  
     {  
+        #region User
         void AddUserRecord(User user);  
         void UpdateUserRecord(User user);  
         void DeleteUserRecord(int id);  
         User GetUserSingleRecord(int id);  
         List<User> GetUserRecords();  
+        #endregion
 
-
+        #region Admin
         void AddAdminRecord(Admin user);  
         void UpdateAdminRecord(Admin user);  
         void DeleteAdminRecord(int id);  
         Admin GetAdminSingleRecord(int id);  
         List<Admin> GetAdminRecords();
+        #endregion
+
     }  
 }  
