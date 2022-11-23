3d3d565759beb1788ca5dd42987f818c6c22a263
tronghuu.dailyxe <tronghuu.dailyxe@outlook.com>
2022-11-23 17:58:36 +0700

    ok

--- APIMyMyStore/Controllers/UserController.cs
+++ APIMyMyStore/Controllers/UserController.cs
@@ -20,6 +20,53 @@ namespace APIMyMyStore.Controllers
 
         protected override List<string> FieldUpdate => new List<string> { "name", "address" };
 
-      
+      [Route("loaddata")]
+        [HttpPost]
+        public override IActionResult LoadData([FromBody] JObject pData)
+        {
+            return Ok(() =>
+             {
+                 List<string> lstFieldSearch = null;
+                 List<object> lstDataSearch = null;
+                 List<string> lstFieldCondition = new List<string>();
+                 List<object> lstDataCondition = new List<object>();
+                 string textSearch = CommonMethods.ConvertToString(pData["textsearch"]).Trim();
+                 string condition = GetCondition(pData["filter"] as JObject);
+                 string conditionParameters = string.Empty;
+                 int from = CommonMethods.ConvertToInt32(pData["from"]);
+                 int to = CommonMethods.ConvertToInt32(pData["to"]);
+                 bool isAsc = CommonMethods.ConvertToBoolean(pData["ordersort"]);
+                 string fieldSort = CommonMethods.ConvertToString(pData["fieldsort"]);
+                 string orderBy = string.Empty;
+                 if (pData.ContainsKey("parameter"))
+                 {
+                     conditionParameters = GetConditionParameters(pData["parameter"] as JObject, ref lstFieldCondition, ref lstDataCondition);
+                 }
+
+
+                 if (string.IsNullOrEmpty(fieldSort))
+                 {
+                     orderBy = this.OrderByGrid;
+                 }
+                 else
+                 {
+                     orderBy = fieldSort + " " + (isAsc ? "ASC" : "DESC");
+                 }
+
+                 if (!string.IsNullOrEmpty(conditionParameters))
+                 {
+                     condition += string.IsNullOrEmpty(condition) ? conditionParameters : " AND " + conditionParameters;
+                 }
+
+                 if (!string.IsNullOrEmpty(textSearch))
+                 {
+                     textSearch = CommonMethods.ConvertUnicodeToASCII(textSearch);
+                     lstFieldSearch = this.FieldSearch.ToList<string>();
+                     lstDataSearch = new List<object> { textSearch };
+                 }
+                 var dataset = GetTemplateDAL(ViewName).SearchData(from, to, orderBy, FieldSelect, condition, lstFieldSearch, lstDataSearch, lstFieldCondition, lstDataCondition);
+                 return DataTableToJSON(dataset.Tables[0]);
+             });
+        }
     }
 }
